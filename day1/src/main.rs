631a9ba6fcc2d0e03d908cb214223aed96bc5940
Oliver Calder <oliver@calder.dev>
2024-12-03 23:18:59 -0600

    day1: refactored into separate functions with unit tests
    Signed-off-by: Oliver Calder <oliver@calder.dev>

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
-    let distance = total_distance(std::io::stdin().lock());
-    println!("{}", distance)
+    let (left, right) = sorted_cols(std::io::stdin().lock());
+    let distance = total_distance(&left, &right);
+    println!("total distance: {}", distance);
 }
 
-fn total_distance(r: impl std::io::BufRead) -> isize {
+fn sorted_cols(r: impl std::io::BufRead) -> (Vec<isize>, Vec<isize>) {
     let (mut left, mut right): (Vec<isize>, Vec<isize>) = r.lines()
                         .map_while(Result::ok)
                         .filter(|line| !line.is_empty())
@@ -11,10 +12,7 @@ fn total_distance(r: impl std::io::BufRead) -> isize {
                         .unzip();
     left.sort_unstable();
     right.sort_unstable();
-    left.into_iter()
-        .zip(right)
-        .map(|(x, y)| (x - y).abs())
-        .sum()
+    (left, right)
 }
 
 fn line_to_tuple(line: &str) -> (isize, isize) {
@@ -23,18 +21,46 @@ fn line_to_tuple(line: &str) -> (isize, isize) {
     pair
 }
 
+fn total_distance(left: &[isize], right: &[isize]) -> isize {
+    left.iter()
+        .zip(right.iter())
+        .map(|(x, y)| (x - y).abs())
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn test_example() {
-        let test_input = std::io::BufReader::new("
+    const EXAMPLE_INPUT: &str = "
 3   4
 4   3
 2   5
 1   3
 3   9
-3   3".as_bytes());
-        let result = super::total_distance(test_input);
+3   3";
+
+    #[test]
+    fn test_sorted_cols() {
+        let test_input = std::io::BufReader::new(EXAMPLE_INPUT.as_bytes());
+        let (left, right) = super::sorted_cols(test_input);
+        assert_eq!(left, vec![1, 2, 3, 3, 3, 4]);
+        assert_eq!(right, vec![3, 3, 3, 4, 5, 9]);
+    }
+
+    #[test]
+    fn test_line_to_tuple() {
+        assert_eq!(super::line_to_tuple("3   4"), (3, 4));
+        assert_eq!(super::line_to_tuple("4   3"), (4, 3));
+        assert_eq!(super::line_to_tuple("2   5"), (2, 5));
+        assert_eq!(super::line_to_tuple("1   3"), (1, 3));
+        assert_eq!(super::line_to_tuple("3   9"), (3, 9));
+        assert_eq!(super::line_to_tuple("3   3"), (3, 3));
+    }
+
+    #[test]
+    fn test_example_distance() {
+        let test_input = std::io::BufReader::new(EXAMPLE_INPUT.as_bytes());
+        let (left, right) = super::sorted_cols(test_input);
+        let result = super::total_distance(&left, &right);
         let expected = 11; // given expected answer
         assert_eq!(result, expected)
     }
