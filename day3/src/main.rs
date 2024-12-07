4732b497c3a138cfaf03b1accae5f3482e829656
Oliver Calder <oliver@calder.dev>
2024-12-07 15:25:04 -0600

    day3: support multiline input and add encrypted puzzle input
    Signed-off-by: Oliver Calder <oliver@calder.dev>

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,21 +1,24 @@
 use regex::Regex;
-use std::io::BufRead;
 
 fn main() -> std::io::Result<()> {
-    let mut buffer = String::new();
-    std::io::stdin().lock().read_line(&mut buffer)?;
-    let result = sum_muls(&buffer);
+    let result = sum_muls(std::io::stdin().lock());
     println!("{}", result);
     Ok(())
 }
 
-fn sum_muls(s: &str) -> usize {
-    let re = Regex::new(r"mul\((?<x>[0-9]+),(?<y>[0-9]+)\)").unwrap();
-    re.captures_iter(s)
-        .map(|caps| {
-            let x: usize = caps.name("x").unwrap().as_str().parse().unwrap();
-            let y: usize = caps.name("y").unwrap().as_str().parse().unwrap();
-            x * y
+fn sum_muls(reader: impl std::io::BufRead) -> usize {
+    let re = Regex::new(r"mul\((?<x>[0-9]([0-9]?)([0-9]?)),(?<y>[0-9]([0-9]?)([0-9]?))\)").unwrap();
+    reader
+        .lines()
+        .map_while(Result::ok)
+        .map(|line| {
+            re.captures_iter(&line)
+                .map(|caps| {
+                    let x: usize = caps.name("x").unwrap().as_str().parse().unwrap();
+                    let y: usize = caps.name("y").unwrap().as_str().parse().unwrap();
+                    x * y
+                })
+                .sum::<usize>()
         })
         .sum()
 }
@@ -25,7 +28,14 @@ mod tests {
     #[test]
     fn test_sum_muls() {
         let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-        let result = super::sum_muls(input);
+        let result = super::sum_muls(std::io::BufReader::new(input.as_bytes()));
         assert_eq!(result, 161);
+    }
+
+    #[test]
+    fn test_sum_muls_multiline() {
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))\nxmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        let result = super::sum_muls(std::io::BufReader::new(input.as_bytes()));
+        assert_eq!(result, 322);
     }
 }
